3384ccd72ee8f18cc8811b9037a52dfe0e62859c
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-03-14 20:34:16 +0700

    Database

---
+++ index.js
@@ -0,0 +1,11 @@
+// Memilih Database
+
+// use belajar         // memilih database belajar
+// use hello           // memilih database hello
+// show databases      // menampilkan semua database
+
+// db.dropDatabase();    // Menghapus database
+// db.getName();         // Mengambil nama database
+// db.hostInfo();        // Mengambil informasi host tempat mongodb
+// db.version();         // Mengambil versi database
+// db.stats();           // Mengambil statistik penggunaan database
