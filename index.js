9f8f31702f459eb526eb60d5289b114adbc98287
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-03-14 20:56:21 +0700

    Insert Document

--- index.js
+++ index.js
@@ -23,6 +23,48 @@
 // db.<collection>.totalSize()  // Mengambil total ukuran collection
 // db.<collection>.stats()      // Mengambil informasi statistik collection
 
-// db.createCollection("customers")  // Membuat collection customers
-// db.createCollection("products")   // Membuat collection products
-// db.createCollection("orders")     // Membuat collection orders
+db.createCollection("customers"); // Membuat collection customers
+db.createCollection("products"); // Membuat collection products
+db.createCollection("orders"); // Membuat collection orders
+
+// Insert Document
+// db.<collection>.insertOne(document)         // Menambah dokumen ke collection
+// db.<collection>.insertMany(array<document>) // Menambah semua dokumen di array ke collection
+
+// insert customers document
+db.customers.insertOne({
+  _id: "Rizal",
+  name: "Rizal",
+});
+
+// insert products document
+db.products.insertMany([
+  {
+    _id: 1,
+    name: "Indomie Ayam Bawang",
+    price: new NumberLong("2000"),
+  },
+  {
+    _id: 2,
+    name: "Mie Sedap Soto",
+    price: new NumberLong("2000"),
+  },
+]);
+
+// insert orders document
+db.orders.insertOne({
+  _id: new ObjectId(),
+  total: new NumberLong("4000"),
+  items: [
+    {
+      product_id: 1,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+    {
+      product_id: 2,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+  ],
+});
